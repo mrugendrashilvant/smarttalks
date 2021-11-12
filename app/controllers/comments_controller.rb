5f2067af703b4481684d4e0e749f843f7da58f07
mrugendrashilvant <sanketshilvant@gmail.com>
2021-11-12 11:48:27 +0530

    ...

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,10 @@
 class CommentsController < ApplicationController
-    def create 
-        @article = Article.find(params[:article_id])
-        @comment = @article.comments.create(comment_params)
-        redirect_to article_path(@article)
+    def create
+        if request.post? 
+            @article = Article.find(params[:article_id])
+            @comment = @article.comments.create(comment_params)
+            redirect_to article_path(@article)
+        end
     end
 
     private 
