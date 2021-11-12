16e971e7abf22421d6483beb65a14e996ae6e221
mrugendrashilvant <sanketshilvant@gmail.com>
2021-11-12 15:27:39 +0530

    added delete to comments, authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,20 @@
 class CommentsController < ApplicationController
+
+    http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
     def create
         if request.post? 
             @article = Article.find(params[:article_id])
             @comment = @article.comments.create(comment_params)
             redirect_to article_path(@article)
         end
+    end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy 
+        redirect_to article_path(@article)
     end
 
     private 
