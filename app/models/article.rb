d9bb953739b35055fa30bb1a47b2e66ffb1899b8
mrugendrashilvant <sanketshilvant@gmail.com>
2021-11-12 15:34:51 +0530

    created model, controller, association for likes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
     has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
     
     validates :title, presence: true
     validates :content, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_020249) do
+ActiveRecord::Schema.define(version: 2021_11_12_100315) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2021_11_12_020249) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "count"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_likes_on_article_id"
   end
 
 end
